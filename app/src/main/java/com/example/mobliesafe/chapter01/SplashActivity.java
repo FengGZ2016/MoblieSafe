b9428d52883c8932597d9302cc88555d60f66e9f
冯国富 <fgz2016@outlook.com>
2017-06-15 18:35:02 +0800

    编写Splash布局

---
+++ app/src/main/java/com/example/mobliesafe/chapter01/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.mobliesafe.chapter01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mobliesafe.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
